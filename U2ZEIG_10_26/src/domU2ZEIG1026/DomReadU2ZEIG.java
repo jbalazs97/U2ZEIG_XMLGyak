a351b8ceeb4bdd8c2ec88ba6a5347705482dc74a
Balázs Juhász <juhaszbalazs855@gmail.com>
2021-10-26 16:34:51 +0200

    xml_gyak_10_26

---
+++ U2ZEIG_10_26/src/domU2ZEIG1026/DomReadU2ZEIG.java
@@ -0,0 +1,42 @@
+package domU2ZEIG1026;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+public class DomReadU2ZEIG {
+        public void readElements(){
+            try
+            {
+                File file = new File("userU2ZEIG.xml");
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(file);
+                doc.getDocumentElement().normalize();
+                System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+                NodeList nodeList = doc.getElementsByTagName("user");
+                for (int itr = 0; itr < nodeList.getLength(); itr++)
+                {
+                    Node node = nodeList.item(itr);
+                    System.out.println("\nNode Name :" + node.getNodeName());
+                    if (node.getNodeType() == Node.ELEMENT_NODE)
+                    {
+                        Element eElement = (Element) node;
+                        System.out.println("User element id: "+ eElement.getAttribute("id"));
+                        System.out.println("First Name: "+ eElement.getElementsByTagName("firstName").item(0).getTextContent());
+                        System.out.println("Last Name: "+ eElement.getElementsByTagName("lastName").item(0).getTextContent());
+                        System.out.println("Profession: "+ eElement.getElementsByTagName("profession").item(0).getTextContent());
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ U2ZEIG_10_26/src/domU2ZEIG1026/Main.java
@@ -0,0 +1,10 @@
+package domU2ZEIG1026;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            DomReadU2ZEIG domReadU2ZEIG = new DomReadU2ZEIG();
+            domReadU2ZEIG.readElements();
+        }
+    }
+
